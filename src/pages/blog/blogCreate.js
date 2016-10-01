093433e56413814c312ab08cbe793bc652139538
zhangpeng <zhangpeng@fenbi.com>
2016-10-01 14:04:52 +0800

    FIX: fix create don't jump bug

--- src/pages/blog/blogCreate.js
+++ src/pages/blog/blogCreate.js
@@ -3,13 +3,15 @@
 app.controller('BlogCreateCtrl', ['$routeParams', '$location', '$route', 'Toast', '$scope', 'Blog', function ($routeParams, $location, $route, Toast, $scope, Blog) {
 
 
+    $scope.blog = {};
     $scope.id = $routeParams.id;
     if ($scope.id){
         $scope.blog = Blog.get({id : $scope.id});
     }
 
     $scope.save = function () {
-        Blog.save($scope.blog).$promise.then(function(){
+        Blog.save($scope.blog).$promise.then(function(blog){
+            $location.url('/blogs/'+blog.id);
             $route.reload();
         } );
     };
